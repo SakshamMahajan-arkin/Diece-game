e732d8e18121b12faa3fa7824603ff5c7ff1ad26
Saksham <sakshammahajan441@gmail.com>
2020-10-18 00:42:07 +0530

    Added 2nd random

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ random=random%7;
 if (random===0){
   random=1;
 }
+document.querySelector('.img1').src="images/dice"+random+".png";
+var random2=7*(Math.random());
+random2=Math.ceil(random2);
+random2=random2%7;
+if (random2===0){
+  random2=1;
+}
+document.querySelector('.img2').src="images/dice"+random2+".png";
